d5efb013aa220d272f55b5ede0188cc84b012fd5
eliottscherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-12-19 09:23:38 +0100

    fix: add health check endpoint for frontend and backend workflows

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,10 @@ const userApi = require('./user.api');
 const authApi = require('./auth.api');
 const todoApi = require('./todo.api');
 
+router.get('/health', (req, res) => {
+  return res.status(200).json({ ok: true });
+});
+
 router.use('/api/user', userApi);
 router.use('/api/auth', authApi);
 router.use('/api/todo', todoApi);
